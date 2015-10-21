a838a0355e9af7bf88eb4919d869f9e7e341f8b0
Moritz Sauter <mosauter@htwg-konstanz.de>
2015-10-21 15:34:57 +0200

    wt: running config

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import de.htwg.battleship.Battleship;
+import de.htwg.battleship.controller.IMasterController;
 import play.*;
 import play.mvc.*;
 
@@ -7,8 +9,19 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public Result index() {
+    static IMasterController controller = Battleship.getInstance().getMaster();
+    static Battleship battleship = Battleship.getInstance();
+    public static Result index() {
         return ok(index.render(about.render("Battleship POW! POW!")));
     }
+
+    //public static Result index() {
+      //  return ok(views.html.index.render("Battleship", controller));
+    //}
+
+    //public static Result commandline(String command) {
+      //  battleship.getTui().processInputLine(command);
+        //return ok(views.html.index.render("Got your command "+ command, controller));
+    //}
 
 }
