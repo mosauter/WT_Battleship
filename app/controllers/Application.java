f8eb9b966dd7ca71bf17f6f391919ba67ef04eac
Moritz Sauter <SauterMoritz@gmx.de>
2015-11-25 20:03:48 +0100

    created a Mock of the WuiController

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,15 +2,14 @@ package controllers;
 
 
 import de.htwg.battleship.Battleship;
-
 import de.htwg.battleship.aview.tui.TUI;
-import play.*;
-import play.mvc.*;
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
 import play.mvc.WebSocket;
-import play.mvc.WebSocket.*;
-import play.libs.F.Callback;
-import play.libs.F.Callback0;
+import views.html.about;
+import views.html.game;
+import views.html.home;
+import views.html.index;
 
 public class Application extends Controller {
 
@@ -24,8 +23,8 @@ public class Application extends Controller {
         TUI tui = bs.getTui();
         tui.processInputLine(command);
         String s = tui.printTUI();
-        String b = s.replaceAll("\n","<br>" );
-        b = b.replaceAll(" ","&nbsp;" );
+        String b = s.replaceAll("\n", "<br>");
+        b = b.replaceAll(" ", "&nbsp;");
 
         return ok(views.html.battleship.render(b));
     }
@@ -40,18 +39,14 @@ public class Application extends Controller {
 
     public WebSocket<String> socket() {
         return new WebSocket<String>() {
-            public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {
-                in.onMessage(new Callback<String>() {
-                    public void invoke(String event) {
-                        System.out.println("Some cool event came: " + event);
-                    }
+            public void onReady(WebSocket.In<String> in,
+                                WebSocket.Out<String> out) {
+                in.onMessage(event -> {
+                    WuiControllerMock wuiControllerMock =
+                        new WuiControllerMock(out);
+                    wuiControllerMock.analyzeMessage(event);
                 });
-                in.onClose(new Callback0() {
-                    public void invoke() {
-                        System.out.println("CLOSING SOCKET");
-                    }
-                });
-                out.write("{\"object\":\"ghjkl\",\"zahl\":14}");
+                in.onClose(() -> System.out.println("CLOSING SOCKET"));
             }
         };
     }
