77371936966f28a66aef70e4d2caf8182b2c6298
Moritz Sauter <SauterMoritz@gmx.de>
2016-01-12 16:11:33 +0100

    feature(hit map): added a real hit map to the shoot message, to determine if a shoot in shootMap was a hit or a miss

--- app/controllers/util/ShootMessage.java
+++ app/controllers/util/ShootMessage.java
@@ -14,6 +14,7 @@ import de.htwg.battleship.util.State;
  *     {
  *         "type": ( SHOOT1 | SHOOT2 ),
  *         "isHitMap": [[]]
+ *         "isShootMap": [[]]
  *     }
  *     }
  * </pre>
@@ -24,9 +25,11 @@ import de.htwg.battleship.util.State;
 public class ShootMessage extends Message {
 
     private final boolean[][] isHitMap;
+    private final boolean[][] isShootMap;
 
-    public ShootMessage(State type, boolean[][] isHitMap) {
+    public ShootMessage(State type, boolean[][] isShootMap, boolean[][] isHitMap) {
         this.type = type.toString();
+        this.isShootMap = isShootMap;
         this.isHitMap = isHitMap;
     }
 }
